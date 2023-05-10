39efeef7751a593958876626dd107ac417e8f2af
Sylvain DELMOTE <s.delmote@cafnord.cnafmail.fr>
2023-05-10 11:04:58 +0200

    Editable block

--- src/components/DocEditor.vue
+++ src/components/DocEditor.vue
@@ -104,12 +104,11 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
   color: black;
   background: rgb(248, 249, 250);
 }
--- src/components/MyEditor.vue
+++ src/components/MyEditor.vue
@@ -2,26 +2,65 @@
 <script setup>
 // IMPORT
 import VueDocumentEditor from 'vue-document-editor'
-import {ref} from 'vue'
+import VueFileToolbarMenu from 'vue-file-toolbar-menu';
+import {ref, computed, onMounted} from 'vue'
 
 // VARIABLES
-const  content= ref( [" <h2> Je suis une zone éditable </h2> Coller du Texte ici"])
-   
+const editableBlock = `<div class='editableBlock' contenteditable='true'> <br> <h3> Editable Block </h3> Please Edith me </div>`
+const  content= ref( ["<div> Coller du texte ici </div>"+editableBlock] )
+const  documentIsEditable = ref(true)
+
+// FUNCTIONS
+const menu =  computed( ()=> {
+  return   [
+    { text: "Nouveau", title: "Nouveau Document", icon: "description", click: () => { if(confirm("Créer un nouveau document ?")){ content.value = [""]; } } },
+    { text:  documentIsEditable.value ? "Verrouiller" :"Deverrouiller", title: "Verrou document" , icon:  documentIsEditable.value ? "lock": "lock_open", click: () => { documentIsEditable.value = !documentIsEditable.value ; console.log(documentIsEditable.value) } },
+  ]
+} )
+
+
 
 
   
 </script>
 
 <template>
-  <div class="container" >
-    <vue-document-editor v-model:content="content" /> 
+  <div class="main" >
+    
+    <vue-file-toolbar-menu 
+      :content="menu" 
+      class="bar"
+    />
+
+    <vue-document-editor 
+      v-model:content= content
+      :editable= "documentIsEditable"      
+    /> 
   </div>
+
+
 </template>
 
 
-<style scoped>
-.container {
-   
-  border: 1px solid black;
+
+
+
+<style>
+.main {
+  width: fit-content;
+  min-width: 100%;
+  max-width: 50vw;
 }
+.bar {
+  position: sticky;
+  left: 0;
+  top: 0;
+  width: calc(50vw );
+  z-index: 1000;
+  background: rgba(248, 249, 250, 0.8);
+  border-bottom: solid 1px rgb(248, 249, 250);
+}
+
+
+    
 </style>
